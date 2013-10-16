8e0ab00d5114130b1ef7795e29f7e580cff80266
Sakti Dwi Cahyono <54krpl@gmail.com>
2013-10-16 11:36:50 +0700

    update code on executor httppost

--- executor/httppost.py
+++ executor/httppost.py
@@ -1,8 +1,22 @@
-"""sample executor"""
+"""http post executor, using requests.
+Set fields with comma separated values"""
+import requests
 
 
-LIST_OPTIONS = ['abcef', 'sdfd']
+LIST_OPTIONS = ['url', 'fields']
 
 
 def execute(options, payloads, logging):
-    pass
+    url = options.get('url')
+    fields = options.get('fields')
+    fields = fields.split(',')
+    data = {}
+    for payload in payloads:
+        for field in fields:
+            data[field] = payload
+        logging.info('payload: %s' % payload)
+        logging.info(data)
+        r = requests.post(url, data=data, verify=False,
+                allow_redirects=False)
+        logging.info('status code: %s' % r.status_code)
+        logging.info('response: %s' % r.text)
